d83514dd3e6a81e64f568deea61d4e6bf936760e
Brian Sayler <sayler.b@gmail.com>
2016-10-05 11:55:28 -0600

    pass some colors as props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,28 +2,24 @@ var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  // two things about render
-  //  -- must return a valid react component
-  //  -- must be pure (a given input will always give the same output)
   render() {
     return (
       div(null,
-        h1(null, this.props.title)
+        h1({style: {color: this.props.color}}, this.props.title)
       )
     )
   }
 })
 
 
-var MyTitleFact = React.createFactory(MyTitle) // we won't this with JSX
+var MyTitleFact = React.createFactory(MyTitle)
 var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    // All these next three lines are equivalent:
-    MyTitleFact({title: 'Props are great!'}),  
-    React.createElement(MyTitle, {title: 'Use props everywhere!'}),
-    ce(MyTitle, {title: 'Props are the best!'})
+    MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
   )
 )
 
