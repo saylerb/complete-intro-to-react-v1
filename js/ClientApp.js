decb4e16a6bc4ed8b0d21b2a503d1fb86049a31a
Brian Sayler <sayler.b@gmail.com>
2016-10-05 11:03:44 -0600

    demonstrate elements inside a component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,25 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+  render() {
+    return (
+      div(null,
+        h1(null, 'Check out this other thing.')
+      )
+    )
+  }
+})
+
+// Components are like instances, that throw elements onto the page
+// can only have a single top-level element (a div
+
 var MyFirstComponent = (
   div(null,
-    h1(null, 'Hello, World! First Component!')
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null),
+    React.createElement(MyTitle, null)
   )
 )
 
